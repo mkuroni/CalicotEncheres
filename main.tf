3f606bd786fa2d84a170bbb294fdcb6ea72d02b8
Sebastien Beaudet <70425216+mkuroni@users.noreply.github.com>
2025-03-15 11:28:24 -0400

    subnet missing stuff

--- main.tf
+++ main.tf
@@ -28,3 +28,14 @@ resource "azurerm_subnet" "snet" {
   ]
 }
 
+resource "azurerm_subnet" "snet" {
+  name                 = "snet-dev-web-cc-11"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
+
