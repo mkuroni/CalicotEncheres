c3c0d01197f47d3c6347fbf97456b89ca5ae1ff9
Sebastien Beaudet <70425216+mkuroni@users.noreply.github.com>
2025-03-15 10:50:13 -0400

    remade

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.23.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
 }
 
-# resource "" "" {
-#     name =
-#     location = ""
-#     tags = {
-#         Environment = ""
-#         Team = ""
-#     }
-# }
+resource "azurerm_resource_group" "rgrg-calicot-web-dev" {
+  name     = "rg-calicot-web-dev-11"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet-dev-calicot-cc" {
+  name                = "vnet-dev-calicot-cc-11"
+  location            = var.location
+  address_space       = [""]
+  resource_group_name = azurerm_resource_group.rgrg-calicot-web-dev
+}
+
+# REQUIS 1
+
+# REQUIS 2
+
+# REQUIS 3
+
+# REQUIS 4
 
 
---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Canada Central everywhere
+variable "location" {
+  default = "cac"
+}
+
+variable "resource_group_name" {
+  default = "rg-calicot-web-dev-11"
+}
+variable "vnet_group_name" {
+  default = "vnet-dev-calicot-cc-11"
+}
+
