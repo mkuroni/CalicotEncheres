4ffab6c3168176b7e3ad30623cf2a2aea5046a3b
Sebastien Beaudet <70425216+mkuroni@users.noreply.github.com>
2025-03-15 12:05:48 -0400

    app debut

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-terraform {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
@@ -28,14 +35,34 @@ resource "azurerm_subnet" "snet" {
   ]
 }
 
-resource "azurerm_subnet" "snet" {
-  name                 = "snet-dev-web-cc-11"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_service_plan" "plan" {
+  name                = "plan-calicot-dev-11"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"
+  sku_name            = "S1"
 
-  depends_on = [
-    azurerm_virtual_network.vnet
-  ]
+
+  maximum_elastic_worker_count = 2
+  app_service_environment_id   = null
 }
+resource "azurerm_windows_web_app" "app" {
+  name                = "app-calicot-dev-11"
+  location            = "Canada Central"
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+  https_only          = false
 
+
+  app_settings = {
+    "ImageUrl" = "https://stcalicotprod000.blob.core.windows.net/images/"
+  }
+
+  site_config {
+
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
